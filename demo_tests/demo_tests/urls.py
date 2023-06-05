7202bb9b601e1674477eddbd6f42354897c40e0b
MrJmad <j-mad@j-mad.com>
2023-06-05 19:16:10 +0200

    fix removal of django.conf.urls.url()

--- demo_tests/demo_tests/urls.py
+++ demo_tests/demo_tests/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+try:
+    from django.conf.urls import url as re_path
+except ImportError:
+    from django.urls import re_path
 from django.contrib import admin
 
 
-urlpatterns = [url(r"^admin/", admin.site.urls)]
+urlpatterns = [re_path(r"^admin/", admin.site.urls)]
